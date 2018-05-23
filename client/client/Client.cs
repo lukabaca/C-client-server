c4c3af777c4b695af7228275541977f188958e15
lukabaca <lkszblaszczyk@gmail.com>
2018-05-23 15:26:28 +0200

    initial commit

---
+++ client/client/Client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Client
+    {
+        private TcpClient clientSocket;
+
+
+        public Client(String serverIpAddress, int portNumber)
+        {
+            clientSocket = new TcpClient();
+            clientSocket.Connect(serverIpAddress, portNumber);
+        }
+        public TcpClient SocketForServer
+        {
+            get
+            {
+                return clientSocket;
+            }
+
+            set
+            {
+                clientSocket = value;
+            }
+        }
+    }
+}
---
+++ server/server/Server.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace server
+{
+    class Server
+    {
+        private int serverPort;
+        private TcpListener serverSocket;
+        private Boolean isRunning;
+
+        public Server(int serverPort)
+        {
+            this.serverPort = serverPort;
+
+            this.serverSocket = new TcpListener(serverPort);
+            this.serverSocket.Start();
+
+            this.isRunning = true;
+
+            Console.WriteLine("Server is working at port: " + serverPort);
+        }
+
+        private void waitForClients()
+        {
+            while(isRunning)
+            {
+                Console.WriteLine("Server is waiting for clients...");
+                TcpClient client = serverSocket.AcceptTcpClient();
+                Console.WriteLine("Client was found: ");
+                Thread thread = new Thread(handleClient);
+                thread.Start(client);
+
+            }
+
+            serverSocket.Stop();
+        }
+
+        private void handleClient(TcpClient client)
+        {
+            TcpClient newClient = client;
+        }
+    }
+}
